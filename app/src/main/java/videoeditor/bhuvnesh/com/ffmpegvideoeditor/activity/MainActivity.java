a397db63ca10391f780693530a9f4b7f1f7c7525
Bilaleng <nicemehar24@gmail.com>
2019-12-06 16:41:59 +0500

    Initial commit

---
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/MainActivity.java
@@ -0,0 +1,532 @@
+package videoeditor.bhuvnesh.com.ffmpegvideoeditor.activity;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import videoeditor.bhuvnesh.com.ffmpegvideoeditor.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_TAKE_GALLERY_VIDEO = 100;
+    private VideoView videoView;
+    private RangeSeekBar rangeSeekBar;
+    private Runnable r;
+    private FFmpeg ffmpeg;
+    private ProgressDialog progressDialog;
+    private Uri selectedVideoUri;
+    private static final String TAG = "BHUVNESH";
+    private static final String POSITION = "position";
+    private static final String FILEPATH = "filepath";
+    private int choice = 0;
+    private int stopPosition;
+    private ScrollView mainlayout;
+    private TextView tvLeft, tvRight;
+    private String filePath;
+    private int duration;
+    private Context mContext;
+    private String[] lastReverseCommand;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+        final TextView uploadVideo = (TextView) findViewById(R.id.uploadVideo);
+        TextView cutVideo = (TextView) findViewById(R.id.cropVideo);
+
+
+
+        tvLeft = (TextView) findViewById(R.id.tvLeft);
+        tvRight = (TextView) findViewById(R.id.tvRight);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        rangeSeekBar = (RangeSeekBar) findViewById(R.id.rangeSeekBar);
+        mainlayout = (ScrollView) findViewById(R.id.mainlayout);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle(null);
+        progressDialog.setCancelable(false);
+        rangeSeekBar.setEnabled(false);
+        loadFFMpegBinary();
+
+        uploadVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 23)
+                    getPermission();
+                else
+                    uploadVideo();
+
+            }
+        });
+
+        cutVideo.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                choice = 2;
+
+                if (selectedVideoUri != null) {
+                    executeCutVideoCommand(rangeSeekBar.getSelectedMinValue().intValue() * 1000, rangeSeekBar.getSelectedMaxValue().intValue() * 1000);
+                } else
+                    Snackbar.make(mainlayout, "Please upload a video", 4000).show();
+            }
+        });
+
+    }
+
+    private void getPermission() {
+        String[] params = null;
+        String writeExternalStorage = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        String readExternalStorage = Manifest.permission.READ_EXTERNAL_STORAGE;
+
+        int hasWriteExternalStoragePermission = ActivityCompat.checkSelfPermission(this, writeExternalStorage);
+        int hasReadExternalStoragePermission = ActivityCompat.checkSelfPermission(this, readExternalStorage);
+        List<String> permissions = new ArrayList<String>();
+
+        if (hasWriteExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permissions.add(writeExternalStorage);
+        if (hasReadExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permissions.add(readExternalStorage);
+
+        if (!permissions.isEmpty()) {
+            params = permissions.toArray(new String[permissions.size()]);
+        }
+        if (params != null && params.length > 0) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    params,
+                    100);
+        } else
+            uploadVideo();
+    }
+
+
+    /**
+     * Handling response for permission request
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    uploadVideo();
+                }
+            }
+            break;
+            case 200: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+            }
+
+
+        }
+    }
+
+    /**
+     * Opening gallery for uploading video
+     */
+    private void uploadVideo() {
+        try {
+            Intent intent = new Intent();
+            intent.setType("video/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Video"), REQUEST_TAKE_GALLERY_VIDEO);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopPosition = videoView.getCurrentPosition(); //stopPosition is an int
+        videoView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.seekTo(stopPosition);
+        videoView.start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO) {
+                selectedVideoUri = data.getData();
+                videoView.setVideoURI(selectedVideoUri);
+                videoView.start();
+
+
+                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        // TODO Auto-generated method stub
+                        duration = mp.getDuration() / 1000;
+                        tvLeft.setText("00:00:00");
+
+                        tvRight.setText(getTime(mp.getDuration() / 1000));
+                        mp.setLooping(true);
+                        rangeSeekBar.setRangeValues(0, duration);
+                        rangeSeekBar.setSelectedMinValue(0);
+                        rangeSeekBar.setSelectedMaxValue(duration);
+                        rangeSeekBar.setEnabled(true);
+
+                        rangeSeekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
+                            @Override
+                            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Object minValue, Object maxValue) {
+                                videoView.seekTo((int) minValue * 1000);
+
+                                tvLeft.setText(getTime((int) bar.getSelectedMinValue()));
+
+                                tvRight.setText(getTime((int) bar.getSelectedMaxValue()));
+
+                            }
+                        });
+
+                        final Handler handler = new Handler();
+                        handler.postDelayed(r = new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (videoView.getCurrentPosition() >= rangeSeekBar.getSelectedMaxValue().intValue() * 1000)
+                                    videoView.seekTo(rangeSeekBar.getSelectedMinValue().intValue() * 1000);
+                                handler.postDelayed(r, 1000);
+                            }
+                        }, 1000);
+
+                    }
+                });
+
+//                }
+            }
+        }
+    }
+
+    private String getTime(int seconds) {
+        int hr = seconds / 3600;
+        int rem = seconds % 3600;
+        int mn = rem / 60;
+        int sec = rem % 60;
+        return String.format("%02d", hr) + ":" + String.format("%02d", mn) + ":" + String.format("%02d", sec);
+    }
+
+    /**
+     * Load FFmpeg binary
+     */
+    private void loadFFMpegBinary() {
+        try {
+            if (ffmpeg == null) {
+                Log.d(TAG, "ffmpeg : era nulo");
+                ffmpeg = FFmpeg.getInstance(this);
+            }
+            ffmpeg.loadBinary(new LoadBinaryResponseHandler() {
+                @Override
+                public void onFailure() {
+                    showUnsupportedExceptionDialog();
+                }
+
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "ffmpeg : correct Loaded");
+                }
+            });
+        } catch (FFmpegNotSupportedException e) {
+            showUnsupportedExceptionDialog();
+        } catch (Exception e) {
+            Log.d(TAG, "EXception no controlada : " + e);
+        }
+    }
+
+    private void showUnsupportedExceptionDialog() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Not Supported")
+                .setMessage("Device Not Supported")
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .create()
+                .show();
+
+    }
+
+    /**
+     * Command for cutting video
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void executeCutVideoCommand(int startMs, int endMs) {
+        File moviesDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_MOVIES
+        );
+
+        String filePrefix = "cut_video";
+        String fileExtn = ".mp4";
+        String yourRealPath = getPath(MainActivity.this, selectedVideoUri);
+        File dest = new File(moviesDir, filePrefix + fileExtn);
+        int fileNo = 0;
+        while (dest.exists()) {
+            fileNo++;
+            dest = new File(moviesDir, filePrefix + fileNo + fileExtn);
+        }
+
+        Log.d(TAG, "startTrim: src: " + yourRealPath);
+        Log.d(TAG, "startTrim: dest: " + dest.getAbsolutePath());
+        Log.d(TAG, "startTrim: startMs: " + startMs);
+        Log.d(TAG, "startTrim: endMs: " + endMs);
+        filePath = dest.getAbsolutePath();
+        //String[] complexCommand = {"-i", yourRealPath, "-ss", "" + startMs / 1000, "-t", "" + endMs / 1000, dest.getAbsolutePath()};
+        String[] complexCommand = {"-ss", "" + startMs / 1000, "-y", "-i", yourRealPath, "-t", "" + (endMs - startMs) / 1000,"-vcodec", "mpeg4", "-b:v", "2097152", "-b:a", "48000", "-ac", "2", "-ar", "22050", filePath};
+
+        execFFmpegBinary(complexCommand);
+
+    }
+
+    /**
+     * Executing ffmpeg binary
+     */
+    private void execFFmpegBinary(final String[] command) {
+        try {
+            ffmpeg.execute(command, new ExecuteBinaryResponseHandler() {
+                @Override
+                public void onFailure(String s) {
+                    Log.d(TAG, "FAILED with output : " + s);
+                }
+
+                @Override
+                public void onSuccess(String s) {
+                    Log.d(TAG, "SUCCESS with output : " + s);
+                    if (choice == 2) {
+                        Intent intent = new Intent(MainActivity.this, PreviewActivity.class);
+                        intent.putExtra(FILEPATH, filePath);
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onProgress(String s) {
+                    Log.d(TAG, "Started command : ffmpeg " + command);
+                    if (choice == 8)
+                        progressDialog.setMessage("progress : splitting video " + s);
+                    else if (choice == 9)
+                        progressDialog.setMessage("progress : reversing splitted videos " + s);
+                    else if (choice == 10)
+                        progressDialog.setMessage("progress : concatenating reversed videos " + s);
+                    else
+                        progressDialog.setMessage("progress : " + s);
+                    Log.d(TAG, "progress : " + s);
+                }
+
+                @Override
+                public void onStart() {
+                    Log.d(TAG, "Started command : ffmpeg " + command);
+                    progressDialog.setMessage("Processing...");
+                    progressDialog.show();
+                }
+
+                @Override
+                public void onFinish() {
+                    Log.d(TAG, "Finished command : ffmpeg " + command);
+                    if (choice != 8 && choice != 9 && choice != 10) {
+                        progressDialog.dismiss();
+                    }
+
+                }
+            });
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            // do nothing for now
+        }
+    }
+
+
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            if (children != null) {
+                for (int i = 0; i < children.length; i++) {
+                    boolean success = deleteDir(new File(dir, children[i]));
+                    if (!success) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri.
+     */
+    private String getDataColumn(Context context, Uri uri, String selection,
+                                 String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+
+}
