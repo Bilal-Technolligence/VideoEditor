90795163bbe4a6d5e9935d69b9d1ca5554e2d4b8
basit96 <pakkobra007@gmail.com>
2019-12-08 01:06:34 +0500

    select + split + save video fully done adding recycler view of videos

--- app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/AllVideosAdapter.java
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/AllVideosAdapter.java
@@ -1,4 +1,65 @@
 package videoeditor.bhuvnesh.com.ffmpegvideoeditor;
 
-public class AllVideosAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AllVideosAdapter extends RecyclerView.Adapter<AllVideosAdapter.ViewHolder> {
+
+    Context context;
+    Activity activity;
+    ArrayList<String> videoUri;
+    ArrayList<String> videoThum;
+    Bitmap bmThumbnail;
+    public AllVideosAdapter(ArrayList<String> videoUri, ArrayList<String> videoThum) {
+                this.videoUri=videoUri;
+                this.videoThum=videoThum;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.allvideos_list,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(videoUri.get(position));
+        //holder.imageView.setImageBitmap(videoThum.get(position));
+
+        //testing
+        bmThumbnail = ThumbnailUtils.createVideoThumbnail(videoUri.get(position), MediaStore.Video.Thumbnails.MICRO_KIND);
+        holder.imageView.setImageBitmap(bmThumbnail);
+        //testing
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoUri.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=(ImageView) itemView.findViewById(R.id.image);
+            textView=(TextView) itemView.findViewById(R.id.tittle);
+        }
+    }
 }
--- app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/ShowAllVideos.java
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/ShowAllVideos.java
@@ -1,14 +1,54 @@
 package videoeditor.bhuvnesh.com.ffmpegvideoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.LayoutManager;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
 
 public class ShowAllVideos extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<String> videoUri = new ArrayList<>();
+    ArrayList<String> videoThum = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_show_all_videos );
+        recyclerView=(RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        fetchVideos();
+
+    }
+
+    private void fetchVideos() {
+    Uri uri;
+    int column_index_data,thum;
+    Cursor cursor;
+    String absolutePathImage=null;
+    uri=MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+    String[] projection={MediaStore.MediaColumns.DATA, MediaStore.Video.Media.BUCKET_DISPLAY_NAME, MediaStore.Video.Media._ID, MediaStore.Video.Thumbnails.DATA};
+    String orderBy=MediaStore.Images.Media.DATE_TAKEN;
+
+    //testing
+        String[] selectedFolder=new String[]{"%Movies%"};
+    cursor=getApplicationContext().getContentResolver().query(uri,projection,null,selectedFolder,orderBy+" DESC");
+        //testing
+
+    column_index_data=cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+    thum=cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
+    while (cursor.moveToNext()){
+        absolutePathImage=cursor.getString(column_index_data);
+        videoUri.add(absolutePathImage);
+        videoThum.add(cursor.getString(thum));
+
+    }
+    recyclerView.setAdapter(new AllVideosAdapter(videoUri,videoThum));
     }
 }
